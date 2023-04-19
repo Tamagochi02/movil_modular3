8e0aef68027556e19856e4e1936bdf96de79cd20
Tamagochi02 <mirytamara@gmail.com>
2023-04-19 08:20:20 -0600

    fix: add api server url

--- lib/pages/registrar/registros_controlador.dart
+++ lib/pages/registrar/registros_controlador.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:movil_modular3/utils/config.dart';
 
 class registerController {
   Future<bool> registrarAlumno(
       String nombre, String correo, String contrasena, String matricula) async {
     final response =
-        await http.post(Uri.parse('http://localhost:3000/api/alumnos'),
+        await http.post(Uri.parse('${Config.ipServerApiUrl}/alumnos'),
             headers: {'Content-Type': 'application/json'},
             body: jsonEncode({
               "nombre": nombre,
@@ -13,20 +14,19 @@ class registerController {
               "contrasena": contrasena,
               "matricula": matricula
             }));
+    print(response.statusCode);
     if (response.statusCode != 200) return false;
     return true;
   }
 
-   Future<bool> registrarDocente(
+  Future<bool> registrarDocente(
       String nombre, String correo, String contrasena) async {
-    final response =
-        await http.post(Uri.parse('http://localhost:3000/api/docentes'),
-            headers: {'Content-Type': 'application/json'},
-            body: jsonEncode({
-              "nombre": nombre,
-              "correo": correo,
-              "contrasena": contrasena
-            }));
+    final response = await http.post(
+        Uri.parse('${Config.ipServerApiUrl}/docentes'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(
+            {"nombre": nombre, "correo": correo, "contrasena": contrasena}));
+    print(response.statusCode);
     if (response.statusCode != 200) return false;
     return true;
   }
