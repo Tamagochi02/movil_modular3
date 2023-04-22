d9f2356d66643fa77aa479b21b1f232c086199ba
Tamagochi02 <mirytamara@gmail.com>
2023-04-22 14:10:35 -0600

    feat: add InfoProjectPage ui

---
+++ lib/pages/alumno/proyecto/infoProyecto_vista.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class InfoProjectPage extends StatefulWidget {
+/*   static const String route = "/infoProject"; */
+  const InfoProjectPage({super.key});
+
+  @override
+  State<InfoProjectPage> createState() => _InfoProjectPageState();
+}
+
+class _InfoProjectPageState extends State<InfoProjectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text("Crear Proyecto",
+            style: TextStyle(color: Color.fromARGB(255, 51, 51, 51))),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.close,
+              color: Color.fromARGB(255, 51, 51, 51),
+            )),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+        child: ListView(children: const [
+          TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                label: Text(
+                  "Correo institucional",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 19,
+                  ),
+                ),
+                suffixIcon: Icon(Icons.mail_outline, color: Colors.white),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                border: OutlineInputBorder(),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+        ]),
+      ),
+    );
+  }
+}
