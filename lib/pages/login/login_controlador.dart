ad38bd536eebfbb49d09b82452555c33be1f565b
Tamagochi02 <mirytamara@gmail.com>
2023-04-25 00:24:07 -0600

    fix: fix endpoints

--- lib/pages/login/login_controlador.dart
+++ lib/pages/login/login_controlador.dart
@@ -10,24 +10,36 @@ class LoginController {
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({'correo': correo, 'contrasena': contrasena}),
     );
+    print(
+        ">> [login] El servidor respondió con un código: ${response.statusCode}");
     if (response.statusCode != 200) return false;
     final cookie = response
         .headers['set-cookie']; // Variable donde guarda la cookie de sesion
     if (cookie == null) return false; // Si no encuentra cookie retorna false
     final pivote =
         cookie.indexOf(";"); // Busca dentro de la cookie el caracter ";"
-    Session().userId = jsonDecode(response.body)['id']; // Guarda el ID del usuario en Session
-    Session().rol = jsonDecode(response.body)['rol']; // Guarda el ROL del usuario en Session
-    Session().nombre = jsonDecode(response.body)['nombre']; // Guarda el ROL del usuario en Session
+    Session().userId =
+        jsonDecode(response.body)['id']; // Guarda el ID del usuario en Session
+    Session().rol = jsonDecode(
+        response.body)['rol']; // Guarda el ROL del usuario en Session
+    Session().nombre = jsonDecode(
+        response.body)['nombre']; // Guarda el ROL del usuario en Session
     Session().cookie = pivote == -1 ? cookie : cookie.substring(0, pivote);
     return true;
   }
 
-  logout() async {
+  Future<bool> logout() async {
     final response = await http.get(
       Uri.parse('${Config.ipServerApiUrl}/logout'),
       headers: {'Content-Type': 'application/json', 'cookie': Session().cookie},
     );
-    print(response.statusCode);
+    print(
+        ">> [logout] El servidor respondió con un código: ${response.statusCode}");
+    if (response.statusCode != 200) return false;
+    Session().cookie = '';
+    Session().userId = '';
+    Session().rol = '';
+    Session().nombre = '';
+    return true;
   }
 }
