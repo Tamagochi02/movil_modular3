03080c013ad5ebc9a0448d42cf57228f4af5a1a7
Tamagochi02 <mirytamara@gmail.com>
2023-04-25 00:48:16 -0600

    fix: add loading screen

--- lib/pages/docente/homeDocente_vista.dart
+++ lib/pages/docente/homeDocente_vista.dart
@@ -15,44 +15,58 @@ class TeacherHomePage extends StatefulWidget {
 class _TeacherHomePageState extends State<TeacherHomePage> {
   final controller = ProjectController();
   List<Project> proyectos = [];
+  bool estaCargando = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    controller
-        .obtenerProyectosPorUsuarioId()
-        .then((proyectos) => setState(() => this.proyectos = proyectos));
+    controller.obtenerProyectosPorUsuarioId().then((proyectos) => setState(() {
+          this.proyectos = proyectos;
+          estaCargando = false;
+        }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Proyectos"),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 52, 105, 221),
-        ),
-        backgroundColor: const Color.fromARGB(215, 255, 255, 255),
-        drawer: const widgets.NavigationDrawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: ListView.builder(
-            itemCount: proyectos.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  ProjectTeacherCard(
-                      nombreProyecto: proyectos[index].nombre,
-                      modulo: proyectos[index].modulo,
-                      estado: proyectos[index].estado,
-                      evaluacion: proyectos[index].evaluacion),
-                  const SizedBox(height: 10)
-                ],
-              );
-            },
-          ),
-        ),
-      );
+      appBar: AppBar(
+        title: const Text("Proyectos"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 52, 105, 221),
+      ),
+      backgroundColor: const Color.fromARGB(215, 255, 255, 255),
+      drawer: const widgets.NavigationDrawer(),
+      body: Stack(
+        children: [
+          if (proyectos.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ListView.builder(
+                itemCount: proyectos.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ProjectTeacherCard(
+                          nombreProyecto: proyectos[index].nombre,
+                          modulo: proyectos[index].modulo,
+                          estado: proyectos[index].estado,
+                          evaluacion: proyectos[index].evaluacion),
+                      const SizedBox(height: 10)
+                    ],
+                  );
+                },
+              ),
+            ),
+          if (estaCargando)
+            Container(
+              color: Colors.white.withOpacity(0.8),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
