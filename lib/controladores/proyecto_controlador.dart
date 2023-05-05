814b9b1e29640c153531cb900a39db41d9cdbdd3
Tamagochi02 <mirytamara@gmail.com>
2023-05-05 07:29:31 -0600

    remove import

--- lib/controladores/proyecto_controlador.dart
+++ lib/controladores/proyecto_controlador.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/rendering.dart';
 import 'package:movil_modular3/modelos/proyecto.dart';
 import 'package:http/http.dart' as http;
 import 'package:movil_modular3/modelos/sesion.dart';
